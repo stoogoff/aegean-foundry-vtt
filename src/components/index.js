6aec99da154a132a507790a15d79b02006a25974
Stoo <stoo.goff@gmail.com>
2023-08-05 17:30:10 +0100

    System setup and cleanup

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-export { default as CharacterSheet } from './CharacterSheet.vue';
-export { default as Editor } from './parts/Editor.vue';
-export { default as Tabs } from './parts/Tabs.vue';
-export { default as Tab } from './parts/Tab.vue';
+export { default as CharacterSheet } from './CharacterSheet.vue'
+export { default as Editor } from './parts/Editor.vue'
+export { default as Tabs } from './parts/Tabs.vue'
+export { default as Tab } from './parts/Tab.vue'
