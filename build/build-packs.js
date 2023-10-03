2973fd51bc147e31b7b9bdcf8d6fbfe719f5b5a1
Stoo <stoo.goff@gmail.com>
2023-10-03 22:19:20 +0100

    Removed packs from repo Fixed typo in system Removed unused code from build-packs

--- build/build-packs.js
+++ build/build-packs.js
@@ -1,56 +1,18 @@
 
-import path from 'node:path'
+import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 import { compilePack } from '@foundryvtt/foundryvtt-cli'
 
-async function run() {
-	const source = path.join(__dirname, '../assets/data/packs/properties')
-	const dest = path.join(__dirname, '../packs/properties')
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-	console.log(source)
+
+async function run() {
+	const source = join(__dirname, '../assets/data/packs/properties')
+	const dest = join(__dirname, '../packs/properties')
+
 	await compilePack(source, dest, { log: true })
-	console.log('Complete?')
 }
 
+// TODO add array of folders to read: ['properties', 'equipment'] etc
 run()
-
-
-/*const fs = require('fs')
-const path = require('path')
-
-const READ_DIR = path.join(__dirname, 'assets/data/packs')
-const WRITE_DIR = path.join(__dirname, 'packs')
-
-const packDirs = fs.readdirSync(READ_DIR)
-
-packDirs.forEach(dir => {
-	const files = fs.readdirSync(path.join(READ_DIR, dir))
-	const output = []
-
-	console.log(`Reading... ${dir}`)
-
-	files.forEach(file => {
-		console.log(`  - Processing ${file}`)
-
-		const content = fs.readFileSync(path.join(READ_DIR, dir, file), 'utf8')
-
-		output.push(JSON.stringify(JSON.parse(content)))		
-	})
-
-	const newFile = path.join(WRITE_DIR, dir + '.db').replace(/\s/g, '\ ')
-
-	if(fs.existsSync(newFile)) {
-		console.log(`${newFile} exists, deleting`)
-		fs.unlinkSync(newFile)
-	}
-
-	console.log(`Writing... ${newFile}`)
-
-	setTimeout(() => fs.writeFileSync(newFile, output.join('\n'), { encoding: 'utf8', flag: 'w' }), 1000)
-})
-*/
-
