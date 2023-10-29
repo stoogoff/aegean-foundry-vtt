eb6400bd4ac2d59f961cdac333df4581e15e72df
Stoo <stoo.goff@gmail.com>
2023-10-29 12:13:36 +0000

    Removed unneeded console.log and bumped version number.

--- module/dialogs/base-roll.js
+++ module/dialogs/base-roll.js
@@ -66,8 +66,6 @@ export const calculateResultAndSendToChat = async (html, context, title) => {
 	// calculate damage if a weapon is included in the context
 	let damage = 0
 
-	console.log(context.weapon)
-
 	if(context.weapon) {
 		damage = parseInt(context.weapon.system.stats.Damage.value) + result.successes
 	}
